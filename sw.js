f9e95674440d9bd9c47eaf59ad241c91884c0fd3
Bijon Guha <iitbguha@gmail.com>
2022-12-26 18:41:18 +0000

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-7755a4dfa00fa844cf77.js"
+    "url": "webpack-runtime-f35c866994eb54402e71.js"
   },
   {
     "url": "framework-0f8de3526885dd073c5e.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "7eb41ec59c2b442b91d68376b3d70482"
+    "revision": "756a4ea0902d09b10d1f01d4a852b3f6"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-f277fb9b28e1661bc3bb.js"
